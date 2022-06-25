8d986936592b56eed6065b18300c8c0d45c70d9a
kgagne15 <kristinagagne@ymail.com>
2022-06-25 12:51:29 -0400

    score almost complete

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask import Flask, render_template, session, request, jsonify
 boggle_game = Boggle()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "jessicachastain"
+# session['score'] = 0
 
 
 @app.route('/')
 def home_page():
     session['board'] = boggle_game.make_board()
+    session['score'] = 0
     return render_template('index.html', board=session['board'])
 
 @app.route('/check-guess')
@@ -20,3 +22,11 @@ def boggle_guess():
     response = boggle_game.check_valid_word(board, word)
     # print(response)
     return  jsonify({'result': response})
+
+@app.route('/score', methods=['POST'])
+def score():
+    # score = session['score']
+    score = request.json['score']
+    session['score'] += score
+    print(session['score'])
+    return jsonify({'score': session['score']})
--- static/script.js
+++ static/script.js
@@ -1,9 +1,41 @@
 console.log('hello')
 
 
+
+
 $('#boggle-form').on('submit', async function(e){
     e.preventDefault()
+
     let word = $('#boggle-guess').val()
-    console.log(word)
+    // console.log(word)
     const res = await axios.get("/check-guess", { params: { word: word }});
+    console.log(res.data.result)
+    let serverResponse = res.data.result
+
+
+    let $msg = `
+    <div id='responseMessage'>
+        ${word}: ${serverResponse}
+    </div>
+    `
+    
+    $('body').append($msg)
+
+    if (serverResponse === 'ok') {
+        submitScore(word)
+    }
+
 })
+
+async function submitScore(word) {
+    let score = word.length;
+    // console.log(score)
+    const res = await axios.post('/score', {score: score})
+    console.log(res.data.score, 'this is javascript')
+    let sessionScore = res.data.score
+    let $item = `
+    <p> Current Boggle Score = ${sessionScore}
+    `
+    $('#boggle-score').append($item)
+    
+}
