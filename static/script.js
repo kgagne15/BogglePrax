9f1b86186209d33971eda3ec76ead42c3135efc0
kgagne15 <kristinagagne@ymail.com>
2022-06-30 16:58:34 -0400

    clean up code

--- static/script.js
+++ static/script.js
@@ -18,35 +18,27 @@ $('#boggle-form').on('submit', async function(e){
     e.preventDefault()
 
     let word = $('#boggle-guess').val()
-    // console.log(word)
     const res = await axios.get("/check-guess", { params: { word: word }});
     console.log(res.data.result)
     let serverResponse = res.data.result
-
-
     let $msg = `
     <div id='responseMessage'>
         ${word}: ${serverResponse}
     </div>
     `
-    
     $('body').append($msg)
-
     if (serverResponse === 'ok') {
         submitScore(word)
     }
-
 })
 
 async function submitScore(word) {
     let score = word.length;
     let highscore = 0; 
-    // console.log(score)
     const res = await axios.post('/score', {score: score})
     console.log(res.data.score, res.data.brokeRecord, res)
     if (res.data.score <= res.data.brokeRecord) {
         let sessionScore = res.data.score
-
         let $item = `
             ${sessionScore}
         `
@@ -56,30 +48,10 @@ async function submitScore(word) {
         console.log('you have reached the else')
         let brokenRecord = res.data.brokeRecord
         console.log('brokenRecord', brokenRecord)
-        // let $item = `
-        //     ${brokenRecord}
-        // `
-        // console.log($item)
         $score.text(brokenRecord)
-        // $('#current-score').append($item)
-
-        // let $newRecord = `
-        // <h2>You broke the highest record!</h2>
-        // `
         $('body').append('<h2>You broke the highest record!</h2>')
         $('#high-score').text(res.data.brokeRecord)
         
     }
-    // console.log(res.data.score, 'this is javascript')
-    // let sessionScore = res.data.score
-
-    // let $item = `
-    //     ${sessionScore}
-    // `
-    // $score.text($item)
-    // let $item = `
-    // <p> Current Boggle Score = ${sessionScore}
-    // `
-    // $('#boggle-score').append($item)
     
 }
